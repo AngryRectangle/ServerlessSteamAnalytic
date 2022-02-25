adbe53834f256d68c95539b44e04134e289721a2
AngryRectangle <yashmel2000@gmail.com>
2022-02-25 12:30:17 +0300

    feat: add game title parsing

---
+++ ServerlessSteamAnalytic/GameInfoModels/Title.cs
@@ -0,0 +1,13 @@
+﻿namespace ServerlessSteamAnalytic.GameInfoModels;
+
+public class Title
+{
+    private readonly string _title;
+
+    public Title(string title)
+    {
+        _title = title;
+    }
+    
+    public static implicit operator string(Title title) => title._title;
+}
---
+++ ServerlessSteamAnalytic/Parsers/IParser.cs
@@ -0,0 +1,9 @@
+﻿using HtmlAgilityPack;
+using ServerlessSteamAnalytic.GameInfoModels;
+
+namespace ServerlessSteamAnalytic.Parsers;
+
+public interface IGameTitleParser
+{
+    Title? ParseGameTitle(HtmlDocument htmlDocument);
+}
