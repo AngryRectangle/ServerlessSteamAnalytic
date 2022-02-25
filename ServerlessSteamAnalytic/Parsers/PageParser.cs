1376be1ac20e86f14f321565dcffb773a4cbf3ea
AngryRectangle <yashmel2000@gmail.com>
2022-02-25 13:56:48 +0300

    chore: rename parser class

---
+++ ServerlessSteamAnalytic/Parsers/PageParser.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+using ServerlessSteamAnalytic.GameInfoModels;
+
+namespace ServerlessSteamAnalytic.Parsers;
+
+public class PageParser : ITitleParser, IPublicationDateParser
+{
+    public Title? ParseGameTitle(HtmlDocument html)
+    {
+        var titleNode = html.DocumentNode.SelectSingleNode("//div[@id='appHubAppName']");
+        return titleNode is null ? null : new Title(titleNode.InnerText);
+    }
+
+    public PublicationDate? ParsePublicationDate(HtmlDocument html)
+    {
+        var releaseDateNode = html.DocumentNode
+            .SelectSingleNode("//div[@class='release_date']")
+            ?.SelectSingleNode("//div[@class='date']");
+        
+        return releaseDateNode is null ? null : new PublicationDate(releaseDateNode.InnerText);
+    }
+}
