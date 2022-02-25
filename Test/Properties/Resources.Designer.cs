2f7e23a062ae10896dce87e3c66c2fcdc36c4b84
AngryRectangle <yashmel2000@gmail.com>
2022-02-25 19:18:34 +0300

    test: add new test game pages for types

--- Test/Properties/Resources.Designer.cs
+++ Test/Properties/Resources.Designer.cs
@@ -67,6 +67,66 @@ namespace Test {
         ///	&lt;meta http-equiv=&quot;Content-Type&quot; content=&quot;text/html; charset=UTF-8&quot;&gt;
         ///			&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width,initial-scale=1&quot;&gt;
         ///		&lt;meta name=&quot;theme-color&quot; content=&quot;#171a21&quot;&gt;
+        ///		&lt;title&gt;Command &amp;amp; Conquer 4: Tiberian Twilight on Steam&lt;/title&gt;
+        ///	&lt;link rel=&quot;shortcut icon&quot; href=&quot;/favicon.ico&quot; type=&quot;image/x-icon&quot;&gt;
+        ///
+        ///	
+        ///	
+        ///	&lt;link href=&quot;https://store.akamai.steamstatic.com/public/shared/css/motiva_sans.css?v=Rc2hpzg2Ex3T&amp;amp;l=english&quot; [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string Command_and_Conquer_4 {
+            get {
+                return ResourceManager.GetString("Command-and-Conquer-4", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;!DOCTYPE html&gt;
+        ///&lt;html class=&quot; responsive&quot; lang=&quot;en&quot;&gt;
+        ///&lt;head&gt;
+        ///	&lt;meta http-equiv=&quot;Content-Type&quot; content=&quot;text/html; charset=UTF-8&quot;&gt;
+        ///			&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width,initial-scale=1&quot;&gt;
+        ///		&lt;meta name=&quot;theme-color&quot; content=&quot;#171a21&quot;&gt;
+        ///		&lt;title&gt;Dark Blood on Steam&lt;/title&gt;
+        ///	&lt;link rel=&quot;shortcut icon&quot; href=&quot;/favicon.ico&quot; type=&quot;image/x-icon&quot;&gt;
+        ///
+        ///	
+        ///	
+        ///	&lt;link href=&quot;https://store.akamai.steamstatic.com/public/shared/css/motiva_sans.css?v=Rc2hpzg2Ex3T&amp;amp;l=english&quot; rel=&quot;stylesheet&quot; type=&quot;text/css [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string Dark_Blood {
+            get {
+                return ResourceManager.GetString("Dark-Blood", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;!DOCTYPE html&gt;
+        ///&lt;html class=&quot; responsive&quot; lang=&quot;en&quot;&gt;
+        ///&lt;head&gt;
+        ///	&lt;meta http-equiv=&quot;Content-Type&quot; content=&quot;text/html; charset=UTF-8&quot;&gt;
+        ///			&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width,initial-scale=1&quot;&gt;
+        ///		&lt;meta name=&quot;theme-color&quot; content=&quot;#171a21&quot;&gt;
+        ///		&lt;title&gt;Dead by Death on Steam&lt;/title&gt;
+        ///	&lt;link rel=&quot;shortcut icon&quot; href=&quot;/favicon.ico&quot; type=&quot;image/x-icon&quot;&gt;
+        ///
+        ///	
+        ///	
+        ///	&lt;link href=&quot;https://store.akamai.steamstatic.com/public/shared/css/motiva_sans.css?v=Rc2hpzg2Ex3T&amp;amp;l=english&quot; rel=&quot;stylesheet&quot; type=&quot;text/ [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string Dead_by_Death {
+            get {
+                return ResourceManager.GetString("Dead-by-Death", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;!DOCTYPE html&gt;
+        ///&lt;html class=&quot; responsive&quot; lang=&quot;en&quot;&gt;
+        ///&lt;head&gt;
+        ///	&lt;meta http-equiv=&quot;Content-Type&quot; content=&quot;text/html; charset=UTF-8&quot;&gt;
+        ///			&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width,initial-scale=1&quot;&gt;
+        ///		&lt;meta name=&quot;theme-color&quot; content=&quot;#171a21&quot;&gt;
         ///		&lt;title&gt;Destiny 2 on Steam&lt;/title&gt;
         ///	&lt;link rel=&quot;shortcut icon&quot; href=&quot;/favicon.ico&quot; type=&quot;image/x-icon&quot;&gt;
         ///
@@ -87,6 +147,26 @@ namespace Test {
         ///	&lt;meta http-equiv=&quot;Content-Type&quot; content=&quot;text/html; charset=UTF-8&quot;&gt;
         ///			&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width,initial-scale=1&quot;&gt;
         ///		&lt;meta name=&quot;theme-color&quot; content=&quot;#171a21&quot;&gt;
+        ///		&lt;title&gt;Dusk Diver 2 on Steam&lt;/title&gt;
+        ///	&lt;link rel=&quot;shortcut icon&quot; href=&quot;/favicon.ico&quot; type=&quot;image/x-icon&quot;&gt;
+        ///
+        ///	
+        ///	
+        ///	&lt;link href=&quot;https://store.akamai.steamstatic.com/public/shared/css/motiva_sans.css?v=Rc2hpzg2Ex3T&amp;amp;l=english&quot; rel=&quot;stylesheet&quot; type=&quot;text/c [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string Dusk_Diver_2 {
+            get {
+                return ResourceManager.GetString("Dusk-Diver-2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;!DOCTYPE html&gt;
+        ///&lt;html class=&quot; responsive&quot; lang=&quot;en&quot;&gt;
+        ///&lt;head&gt;
+        ///	&lt;meta http-equiv=&quot;Content-Type&quot; content=&quot;text/html; charset=UTF-8&quot;&gt;
+        ///			&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width,initial-scale=1&quot;&gt;
+        ///		&lt;meta name=&quot;theme-color&quot; content=&quot;#171a21&quot;&gt;
         ///		&lt;title&gt;Pre-purchase ELDEN RING on Steam&lt;/title&gt;
         ///	&lt;link rel=&quot;shortcut icon&quot; href=&quot;/favicon.ico&quot; type=&quot;image/x-icon&quot;&gt;
         ///
@@ -97,6 +177,26 @@ namespace Test {
         internal static string Elden_Ring {
             get {
                 return ResourceManager.GetString("Elden-Ring", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;!DOCTYPE html&gt;
+        ///&lt;html class=&quot; responsive&quot; lang=&quot;en&quot;&gt;
+        ///&lt;head&gt;
+        ///	&lt;meta http-equiv=&quot;Content-Type&quot; content=&quot;text/html; charset=UTF-8&quot;&gt;
+        ///			&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width,initial-scale=1&quot;&gt;
+        ///		&lt;meta name=&quot;theme-color&quot; content=&quot;#171a21&quot;&gt;
+        ///		&lt;title&gt;Save 25% on Hokko Life on Steam&lt;/title&gt;
+        ///	&lt;link rel=&quot;shortcut icon&quot; href=&quot;/favicon.ico&quot; type=&quot;image/x-icon&quot;&gt;
+        ///
+        ///	
+        ///	
+        ///	&lt;link href=&quot;https://store.akamai.steamstatic.com/public/shared/css/motiva_sans.css?v=Rc2hpzg2Ex3T&amp;amp;l=english&quot; rel=&quot;stylesheet&quot; ty [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string Hokko_Life {
+            get {
+                return ResourceManager.GetString("Hokko-Life", resourceCulture);
             }
         }
         
@@ -167,6 +267,26 @@ namespace Test {
         ///	&lt;meta http-equiv=&quot;Content-Type&quot; content=&quot;text/html; charset=UTF-8&quot;&gt;
         ///			&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width,initial-scale=1&quot;&gt;
         ///		&lt;meta name=&quot;theme-color&quot; content=&quot;#171a21&quot;&gt;
+        ///		&lt;title&gt;NEW LIFE on Steam&lt;/title&gt;
+        ///	&lt;link rel=&quot;shortcut icon&quot; href=&quot;/favicon.ico&quot; type=&quot;image/x-icon&quot;&gt;
+        ///
+        ///	
+        ///	
+        ///	&lt;link href=&quot;https://store.akamai.steamstatic.com/public/shared/css/motiva_sans.css?v=Rc2hpzg2Ex3T&amp;amp;l=english&quot; rel=&quot;stylesheet&quot; type=&quot;text/css&quot;  [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string New_Life {
+            get {
+                return ResourceManager.GetString("New-Life", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;!DOCTYPE html&gt;
+        ///&lt;html class=&quot; responsive&quot; lang=&quot;en&quot;&gt;
+        ///&lt;head&gt;
+        ///	&lt;meta http-equiv=&quot;Content-Type&quot; content=&quot;text/html; charset=UTF-8&quot;&gt;
+        ///			&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width,initial-scale=1&quot;&gt;
+        ///		&lt;meta name=&quot;theme-color&quot; content=&quot;#171a21&quot;&gt;
         ///		&lt;title&gt; on Steam&lt;/title&gt;
         ///	&lt;link rel=&quot;shortcut icon&quot; href=&quot;/favicon.ico&quot; type=&quot;image/x-icon&quot;&gt;
         ///
@@ -197,6 +317,26 @@ namespace Test {
         internal static string Texture_Assembler {
             get {
                 return ResourceManager.GetString("Texture-Assembler", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;!DOCTYPE html&gt;
+        ///&lt;html class=&quot; responsive&quot; lang=&quot;en&quot;&gt;
+        ///&lt;head&gt;
+        ///	&lt;meta http-equiv=&quot;Content-Type&quot; content=&quot;text/html; charset=UTF-8&quot;&gt;
+        ///			&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width,initial-scale=1&quot;&gt;
+        ///		&lt;meta name=&quot;theme-color&quot; content=&quot;#171a21&quot;&gt;
+        ///		&lt;title&gt;Thief Simulator VR on Steam&lt;/title&gt;
+        ///	&lt;link rel=&quot;shortcut icon&quot; href=&quot;/favicon.ico&quot; type=&quot;image/x-icon&quot;&gt;
+        ///
+        ///	
+        ///	
+        ///	&lt;link href=&quot;https://store.akamai.steamstatic.com/public/shared/css/motiva_sans.css?v=Rc2hpzg2Ex3T&amp;amp;l=english&quot; rel=&quot;stylesheet&quot; type=&quot; [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string Thief_Simulator_VR {
+            get {
+                return ResourceManager.GetString("Thief-Simulator-VR", resourceCulture);
             }
         }
     }
